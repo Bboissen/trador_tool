2df322742ac7cfa300b7572fc2e45261935f447e
Bboissen <33459312+Bboissen@users.noreply.github.com>
2025-10-31 16:27:28 +0700

    docs: aggregating API documentation from venue

--- api-gateway/embed_openapi.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import _ "embed"
-
-//go:embed openapi/public-api.yaml
-var openAPIPublicYAML []byte
-
-//go:embed openapi/private-api.yaml
-var openAPIPrivateYAML []byte
--- api-gateway/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/stashfi/api-gateway
-
-go 1.25.0
